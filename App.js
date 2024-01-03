d2afae0376a22abc88729d5cc82e7c417addf7fd
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2024-01-03 14:27:28 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import { StatusBar } from "expo-status-bar";
+import {
+    Button,
+    Platform,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
+import { useState } from "react";
+import Header from "./src/components/Header";
+/*
+tiempo de cursado desde el video:
+1:18:40
+
+*/
+const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
+
+export default function App() {
+    const [isWorking, setIsWorking] = useState(false);
+    const [time, setTime] = useState(25 * 60);
+    const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+    console.log(currentTime);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={{ paddingTop: Platform.OS === "android" && 30 }}>
+                <Text style={styles.text}>Pomodoro</Text>
+                <Text style={styles.text}>{time}</Text>
+
+                <Header
+                    currentTime={currentTime}
+                    setCurrentTime={setCurrentTime}
+                    setTime={setTime}
+                />
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    text: {
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+});
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+﻿import { View,Text,TouchableOpacity,StyleSheet } from "react-native"
+
+const options=["Pomodoro","Short Break","Long Break"];
+
+export default function Header({currentTime,setCurrentTime, setTime}){
+    function handlePress(index){
+        const newTime = index === 0 ? 25 :index === 1 ? 5 : 15;
+        setCurrentTime(index);
+        setTime(newTime*60);
+
+    }
+    return (
+    <View style={{ flexDirection:"row"}}>
+        {options.map((item,index)=>(
+            <TouchableOpacity 
+            key={index}
+            onPress={() => handlePress(index)}
+            style={[styles.itemStyles,currentTime!==index && {borderColor:"transparent"}]}
+            >
+                <Text>
+                    {item}
+                </Text>
+            </TouchableOpacity>
+        ))}
+        {/* <Text>{time}</Text> */}
+    </View>
+    );
+}
+
+const styles=StyleSheet.create({
+    itemStyles:{
+        width: "33%",
+        borderWidth: 3,
+        padding:5,
+    }
+})
