28df00d59d73b1abca34ad5a6305dc6109c56255
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2024-01-03 15:39:40 -0300

    Compilint, ChangeColor with selection

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
     console.log(currentTime);
 
     return (
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView style={[styles.container, {backgroundColor: colors[currentTime] }]}>
             <View style={{ paddingTop: Platform.OS === "android" && 30 }}>
                 <Text style={styles.text}>Pomodoro</Text>
                 <Text style={styles.text}>{time}</Text>
