e3af089fc2838d22be9ed513aa5459be1bd1d8aa
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2024-01-03 18:21:37 -0300

    Se agrego formateo de minutos

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,10 +1,10 @@
 import { View,Text,TouchableOpacity,StyleSheet } from "react-native"
 
 export default function Timer({time}){
-  
+   const formattedTime = `${Math.floor(time/60).toString().padStart(2,"0")}`
    return(
       <View style={styles.container}>
-         <Text style={styles.time}>{time}</Text>
+         <Text style={styles.time}>{formattedTime}</Text>
       </View>
 
    );
